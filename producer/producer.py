9934e8e841d197980c092ddd0725f525b3f48edd
nive404 <nive129persona@gmail.com>
2024-04-04 22:27:31 +0530

    changes made to task 1 and 2

--- producer/producer.py
+++ producer/producer.py
@@ -1,7 +1,7 @@
 import pika
 
 # Establish connection to RabbitMQ server
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 # Declare a queue
@@ -9,6 +9,6 @@ channel.queue_declare(queue='inventory_queue')
 
 # Publish a message to the queue
 channel.basic_publish(exchange='', routing_key='inventory_queue', body='Message from producer')
-
+print(" [x] Sent 'Hello World!'")
 # Close connection
 connection.close()
